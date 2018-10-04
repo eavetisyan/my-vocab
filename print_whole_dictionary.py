4b984e475ad6e6db2a4fa2f72151990e588acb8b
eavetisyan <ed.avetisyan95@gmail.com>
2018-10-04 14:44:12 +0300

    Updated for working with CSV

--- print_whole_dictionary.py
+++ print_whole_dictionary.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 30 14:03:51 2018
+Created on Thu Oct  4 14:26:34 2018
 
 @author: Eduard Avetisyan (ed.avetisyan95@gmail.com)
 ----------------
 Auxiliary application that prints your current dictionary
+Ver 2.0
 """
 
-import shelve
-with shelve.open('lib/dict') as d:
-    a = dict(sorted(d.items()))
-    for s in a:
-        print(s + ' : ' + a[s], end = '')
-    print('--------------------\nTotal: ' + str(len(a)))
+import csv
+from itertools import islice
+
+WORD = 0
+EXPLANATION = 1
+
+with open("lib/dict.csv") as f:
+    csv_file = csv.reader(f)
+    next(islice(csv_file, 3, None))
+    dictionary = [[line[WORD], line[EXPLANATION]] for line in csv_file]
+if dictionary:
+    dictionary.sort()
+    for line in dictionary:
+        print(line[WORD], ":", line[EXPLANATION])
+print("------------------\ntotal:", len(dictionary))
 input()
