6b94c6979c35557cacffc70f243bebce54cc9160
eavetisyan <ed.avetisyan95@gmail.com>
2018-10-06 12:52:49 +0300

    Fixed the stats creation procedure

--- shelve_to_csv.py
+++ shelve_to_csv.py
@@ -5,6 +5,7 @@ Created on Thu Oct  4 18:03:08 2018
 @author: Eduard Avetisyan (ed.avetisyan95@gmail.com)
 ----------------
 From shelve to dictionary converter
+Ver 1.2
 """
 import shelve, csv
 
@@ -12,12 +13,17 @@ WORD = 0
 EXPLANATION = 1
 
 with shelve.open("lib/dict") as shelve_dictionary:
-    with open("lib/dict.csv", "w") as csv_file:
-        stats_header = [["Max guesses", "Min guesses", "Quantity of maximals", "Quantity of minimals"]]
-        empty_row = [[]]
-        dictionary_header = [["Word", "Explanation", "Guess"]]                                                                                                     # during the opening CSV-file
-        header = stats_header + [[0, 0, 0, 0]] + empty_row + dictionary_header
-        csv_dictionary = csv.writer(csv_file, delimiter = ",", lineterminator = "\n")
-        csv_dictionary.writerows(header)
-        for line in shelve_dictionary.items():
-            csv_dictionary.writerow([line[WORD], line[EXPLANATION], 0])
+    try:
+        csv_file = open("lib/dict.csv", "r")
+        csv_file.close()
+        input("The CSV-file is already exists")
+    except IOError:
+        with open("lib/dict.csv", "w") as csv_file:
+            stats_header = [["Max guesses", "Min guesses", "Quantity of maximals", "Quantity of minimals"]]
+            empty_row = [[]]
+            dictionary_header = [["Word", "Explanation", "Guess"]]                                                                                                     # during the opening CSV-file
+            header = stats_header + [[0, 0, len(shelve_dictionary), len(shelve_dictionary)]] + empty_row + dictionary_header
+            csv_dictionary = csv.writer(csv_file, delimiter = ",", lineterminator = "\n")
+            csv_dictionary.writerows(header)
+            for line in shelve_dictionary.items():
+                csv_dictionary.writerow([line[WORD], line[EXPLANATION], 0])
